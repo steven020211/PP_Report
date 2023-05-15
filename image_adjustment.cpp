d4a3a13ee34527d6d18bf443efff3e7b4154269e
steven020211 <steven020211@gmail.com>
2023-05-15 16:49:35 +0800

    adjust code layout for slide

--- image_adjustment.cpp
+++ image_adjustment.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-void adjust_attributes(uint8_t *data, int width, int height, image_attributes &attributes)
+void adjust_attributes(uint8_t *data, int width, int height,
+                       image_attributes &attributes)
 {
     for (int y = 0; y < height; y++)
-    {
         for (int x = 0; x < width; x++)
         {
             uint8_t *pixel = data + (y * width + x) * 3;
             adjust_pixel(pixel, attributes);
         }
-    }
 }
